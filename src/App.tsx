8d33217fb09e7fcb8b8defce0788bd4a7aede9c2
Anna Soboleva <sithhappens1138@gmail.com>
2024-08-27 14:59:22 +0300

    The first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+
+	return (
+		<div className="App">
+			<h1>TO-DO-APP</h1>
+			
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/ToDoItem.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const ToDoItem: FC = () => {
+	return (
+		<div>
+
+		</div>
+	);
+}
+
+export default ToDoItem;

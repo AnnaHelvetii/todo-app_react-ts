3ba7def8926e57bb9b21d19315987f3409191227
Anna Soboleva <sithhappens1138@gmail.com>
2024-09-01 13:10:06 +0300

    Commit_5. LocalStorage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Todo from './models/Todo';
 import ToDoList from './components/ToDoList';
 import ToDoAddForm from './components/ToDoAddForm';
@@ -7,25 +7,40 @@ import './App.css';
 const App: FC = () => {
 	const [todos, setTodos] = useState<Todo[]>([]);
 
-    const addTodo = (title: string) => {
-        const newTodo: Todo = {
+	const addTodo = (title: string) => {
+		const newTodo: Todo = {
             id: Date.now(),
             title: title,
             complete: false,
         };
 
-        setTodos([...todos, newTodo]);
+		const newTodosList: Todo[] = [...todos, newTodo];
+		setTodos(newTodosList);
+
+		localStorage.setItem('todosState', JSON.stringify(newTodosList));
     }
 
     const deleteTodo = (id: number) => {
-        setTodos(todos.filter(todo => todo.id !== id));
+		const newTodosList: Todo[] = todos.filter(todo => todo.id !== id);
+        setTodos(newTodosList);
+
+		localStorage.setItem('todosState', JSON.stringify(newTodosList));
     };
 
 	const toggleTodoComplete = (id: number) => {
-		setTodos(todos.map(todo =>
-			todo.id === id ? {...todo, complete: !todo.complete} : todo
-		));
+		const newTodosList: Todo[] = todos.map(todo =>
+			todo.id === id ? {...todo, complete: !todo.complete} : todo);
+		setTodos(newTodosList);
+
+		localStorage.setItem('todosState', JSON.stringify(newTodosList));
 	};
+
+	useEffect(() => {
+		const todosState: string | null = localStorage.getItem('todosState');
+		if (todosState) {
+			setTodos(JSON.parse(todosState));
+		}
+	}, []);
 
 	return (
 		<div className="App">
